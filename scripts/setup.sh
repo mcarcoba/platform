caad6e38143143ddb3195a0f542e1c770aaed656
mcarcoba <mikel_carcoba@hotmail.com>
2024-11-18 18:56:57 +0100

    wip

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
 curl -sfL https://get.k3s.io | sh -
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.crds.yaml # TODO to yaml
-kubectl create secret generic cloudflare-api-token-secret --from-literal=api-token="$CLOUDFLARE_TOKEN" --namespace=cert-manager # TODO to yaml
 flux bootstrap github --owner=${GITHUB_USER} --repository=platform --branch=main --personal --path=clusters/mcarcoba
+kubectl create secret generic cloudflare-api-token-secret --from-literal=api-token="$CLOUDFLARE_TOKEN" --namespace=cert-manager # TODO to yaml
