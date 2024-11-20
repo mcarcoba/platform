77ea273171873496588ee7565109725ad68986ba
mcarcoba <mikel_carcoba@hotmail.com>
2024-11-20 22:20:49 +0100

    wip

--- scripts/get-client-cert.sh
+++ scripts/get-client-cert.sh
@@ -1,3 +1,3 @@
-kubectl get internal-service-cert-secret -n cert-manager -o jsonpath='{.data.tls\.crt}' | base64 -d > client.crt
-kubectl get internal-service-cert-secret -n cert-manager -o jsonpath='{.data.tls\.key}' | base64 -d > client.key
-kubectl get internal-service-cert-secret -n cert-manager -o jsonpath='{.data.ca\.crt}' | base64 -d > ca.crt
+kubectl get secret internal-service-cert-secret -n cert-manager -o jsonpath='{.data.tls\.crt}' | base64 -d > client.crt
+kubectl get secret internal-service-cert-secret -n cert-manager -o jsonpath='{.data.tls\.key}' | base64 -d > client.key
+kubectl get secret internal-service-cert-secret -n cert-manager -o jsonpath='{.data.ca\.crt}' | base64 -d > ca.crt
