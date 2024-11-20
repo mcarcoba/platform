b3cba1aecbb8313326304d83acd99a9b77839312
mcarcoba <mikel_carcoba@hotmail.com>
2024-11-20 22:55:30 +0100

    wip

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,11 +2,11 @@
 
 # Check if the config file exists
 if [ ! -f ".env" ]; then
-  echo "Error: config.env file not found in the current directory."
+  echo "Error: .env file not found in the current directory."
   exit 1
 fi
 
 # Load environment variables from the config.env file
 export $(cat .env | grep -v '^#' | xargs)
 
-echo "Environment variables loaded from config.env."
+echo "Environment variables loaded from .env."
